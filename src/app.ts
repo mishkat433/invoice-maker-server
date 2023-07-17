a95868cba9cd30c22f897aaefd58bdc28490088f
Mishkat433 <mishkat433@gmail.com>
2023-07-17 16:54:09 +0600

    eslint and husky setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
-import express = require('express')
+import express from 'express'
 const app = express();
+import cors from "cors";
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Server is running')
